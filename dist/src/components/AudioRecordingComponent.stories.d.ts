734897cbcfcb57f63e07ba77ae78cbce7fcb14ec
Ryan Lelek <ryanlelek@gmail.com>
2024-06-22 23:14:23 -0500

    Generate: dist

---
+++ dist/src/components/AudioRecordingComponent.stories.d.ts
@@ -0,0 +1,7 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { default as AudioRecorder } from './AudioRecordingComponent';
+
+declare const meta: Meta<typeof AudioRecorder>;
+export default meta;
+type Story = StoryObj<typeof AudioRecorder>;
+export declare const Primary: Story;
